153e16704363c35ea0ac3f35787a0d1c54f97842
Yadagalla Jaswanth Kumar <yadagallajaswanthkumar98@gmail.com>
2017-03-15 21:28:08 +0530

    Add files via upload

---
+++ 03.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+auto println = []<typename T> (T msg){
+	cout << msg << endl;
+};
+
+int main(){
+	auto a = 10;
+	println(a);
+	println(78.907);
+	return 0;
+}
